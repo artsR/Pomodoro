fe08aa7514c55d94ce0767fa5d575c81ad5b43b7
artur <asihdi@gmail.com>
2020-06-23 12:34:30 +0200

    pomodoro-eagle v1.0.1

--- main.js
+++ main.js
@@ -123,10 +123,12 @@ function createSettingsWindow() {
 }
 
 function runServer() {
-    let FILE_TO_EXEC = 'server'
-    
+    let FILE_TO_EXEC = 'server.py'
+
     let options = {
-        pythonPath: `./pomodoro/${FILE_TO_EXEC}`
+        // pythonPath: `./pomodoro/${FILE_TO_EXEC}`
+        pythonPath: '/home/artur/.local/share/virtualenvs/pomodoro-Wrw1lBnB/bin/python3.8',
+        scriptPath: './pomodoro/',
     }
-    pyshell = PythonShell.run('server', options, (err) => console.log(err))
-    }
+    pyshell = PythonShell.run(`${FILE_TO_EXEC}`, options, (err) => console.log(err))
+}
