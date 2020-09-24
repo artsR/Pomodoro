0721b01b4a8ac06683fff39b1949ce2fcc419575
artur <asihdi@gmail.com>
2020-09-24 18:54:48 +0200

    run server.py instead of executable server

--- main.js
+++ main.js
@@ -123,13 +123,13 @@ function createSettingsWindow() {
 }
 
 function runServer() {
-    // let FILE_TO_EXEC = 'server.py'
+    let FILE_TO_EXEC = 'server.py'
     //
-    // let options = {
-    //     // pythonPath: `./pomodoro/${FILE_TO_EXEC}`,
-    //     pythonPath: '/home/artur/.local/share/virtualenvs/pomodoro-Wrw1lBnB/bin/python3.8',
-    //     scriptPath: './pomodoro/',
-    // }
-    // pyshell = PythonShell.run(`${FILE_TO_EXEC}`, options, (err) => console.log(err))
-    pyshell = require('child_process').execFile(path.join(__dirname, 'pomodoro', 'server'))
+    let options = {
+        // pythonPath: `./pomodoro/${FILE_TO_EXEC}`,
+        // pythonPath: '/home/artur/.local/share/virtualenvs/pomodoro-Wrw1lBnB/bin/python3.8',
+        scriptPath: path.join(__dirname, './pomodoro/'),
+    }
+    pyshell = PythonShell.run(`${FILE_TO_EXEC}`, options, err => console.log(err))
+    // pyshell = require('child_process').execFile(path.join(__dirname, 'pomodoro', 'server'))
 }
